90d1a26598273dce485636e4d30ebf2ab2a6adbc
leandrocm86 <leandrocm86@gmail.com>
2025-04-14 11:27:14 -0300

    Adding common module bundles for CLI and typed scripts

---
+++ commons_cli.py
@@ -0,0 +1,6 @@
+# Common modules bundle for CLI scripts.
+import sys
+import os
+import time
+import argparse
+from pathlib import Path
---
+++ commons_type.py
@@ -0,0 +1,6 @@
+# Common modules bundle for typed scripts.
+from datetime import datetime, timedelta, date
+from typing import TypeVar, TypeAlias, Self, Any
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from enum import Enum

4af98f1522c3c309083cb4eb4dc548b681fdab11
leandrocm86 <leandrocm86@gmail.com>
2025-08-17 11:48:22 -0300

    Making push executable

--- push.py
+++ push.py
@@ -14,14 +14,22 @@ def push(msg: str, url=NUC_URL, warning=False):
 
 def push_external(msg: str, url=NUC_EXTERNAL_URL, warning=False):
     from mods.chaveiro import decrypt_aes
-    PUSH_EXTERNAL_URL = decrypt_aes(environ['PUSH_EXTERNAL_URL'], environ['CHAVEIRO_PYTHONS'])
+    PUSH_EXTERNAL_URL = decrypt_aes(environ['PUSH_EXTERNAL_URL_PYTHONS'], environ['CHAVEIRO_PYTHONS'])
     _push(msg, url, PUSH_EXTERNAL_URL, warning)
 
 
 def _push(msg: str, click_url: str, push_url: str, warning: bool):
-    if not click_url.startswith('http://'):
+    if click_url and not click_url.startswith('http://'):
         click_url = 'http://' + click_url
-    headers = {"Click": click_url}
+    headers = {"Click": click_url} if click_url else {}
     if warning:
         headers.update(WARN_HEADERS)
     requests.post(push_url, data=msg.encode('utf-8'), headers=headers)
+
+
+if __name__ == '__main__':
+    import sys
+    msg = sys.argv[1]
+    url = sys.argv[2]
+    _push(msg, '',  url, False)
+

c1a89022064bf874e4dadf588965f23f63756316
leandrocm86 <leandrocm86@gmail.com>
2025-09-16 18:17:37 -0300

    More common types

--- commons_type.py
+++ commons_type.py
@@ -2,12 +2,14 @@
 from datetime import datetime, timedelta, date  # type: ignore  # noqa
 from typing import Annotated, Type, TypeVar, TypeAlias, Self, Any, Mapping as Map, Sequence as Seq, Set  # type: ignore  # noqa
 from abc import ABC, abstractmethod  # type: ignore  # noqa
+from typing import Iterable  # noqa
 from dataclasses import dataclass, field  # type: ignore  # noqa
 from enum import Enum  # type: ignore  # noqa 
 from collections import defaultdict  # type: ignore  # noqa
 from pathlib import Path  # type: ignore  # noqa
 from typing import Callable  # type: ignore  # noqa
 
+from mods import type_checker  # noqa
 from mods.type_checker import check_type, valint, valbool, valfloat, valstr, valseq, valset, valmap, valpath, valobj  # type: ignore  # noqa
 from mods.mut import mut  # type: ignore  # noqa
 
